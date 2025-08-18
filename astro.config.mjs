63a592fa0d186984422a16e0c3d671a67e6db1c8
Frontendland <ferenc@frontendland.com>
2025-08-18 10:04:07 +0200

    ⬆️ Update Webcore to latest version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
+import { webcore } from 'webcoreui/integration'
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        ssr: {
-            noExternal: ['webcoreui']
-        }
-    }
+    integrations: [webcore()]
 })
