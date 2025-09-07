45796ef88dee201f9f0d257fab90bd3d1128af60
w-barros <woodybarros@hotmail.com>
2025-09-07 04:52:45 -0300

    tailwind added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import { webcore } from 'webcoreui/integration'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [webcore()]
+    integrations: [webcore()],
+      vite: {
+    plugins: [tailwindcss()],
+  },
 })
